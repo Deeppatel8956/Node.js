50f3a3edc2cbeee855a2e1632abf603fb71fc5ec
Deeppatel8956 <110016757+Deeppatel8956@users.noreply.github.com>
2024-06-19 17:24:33 +0530

    understanding  npm install and modules

---
+++ 02understandingserver/script.js
@@ -0,0 +1,12 @@
+// NPM understanding
+// installing and understanding anything basic & advanced
+// npm i one-liner-joke
+// npm uninstall one-liner-joke
+// version install -> npm i accessibility@3.0.9
+
+
+// understanding node modules
+
+// dependenci->pakage and module
+// devdependenci -> it is use to deploy app
+// script-understanding default scripts PATH and custom scripts
